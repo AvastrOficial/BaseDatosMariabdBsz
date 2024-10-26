b8e7262bd0c4ceec50b8ed8edd7dd7bca737a818
AvaStrOficial <91764815+AvastrOficial@users.noreply.github.com>
2024-10-26 11:48:19 -0600

    Create tabal.sql

---
+++ tabal.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE nombre_de_la_base_datos;
+
+SHOW DATABASES;
+
+USE nombre_de_la_base_datos;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    age INT
+);
+
+EXIT;
